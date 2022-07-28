3d779af0b1493a042dcd847af9fc8ac0def7d462
kimdong <parktest0325@gmail.com>
2022-07-29 00:50:08 +0900

    raycast bug fix

--- engine/ray_cast.c
+++ engine/ray_cast.c
@@ -6,7 +6,7 @@
 /*   By: dongkim <dongkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 22:51:26 by dongkim           #+#    #+#             */
-/*   Updated: 2022/07/28 23:56:19 by dongkim          ###   ########.fr       */
+/*   Updated: 2022/07/29 00:48:08 by dongkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,28 +25,32 @@ static void	set_ray_dir(t_raycast *rc, double radian)
 		rc->dir = 1;
 }
 
-static void	init_rc(t_raycast *rc, t_player_data *player, double radian)
+static void	init_rc(t_raycast *rc, t_player_data *player, double *radian)
 {
+	if (*radian < 0)
+		*radian += PI * 2;
+	if (*radian > PI * 2)
+		*radian -= PI * 2;
 	rc->x_len = player->pos.x - (int)player->pos.x;
 	rc->y_len = player->pos.y - (int)player->pos.y;
 	rc->x_step = 0;
 	rc->y_step = 0;
 	rc->x_dir = -1;
 	rc->y_dir = -1;
-	if (PI / 2 > radian || radian > (PI / 2) * 3)
+	if (PI / 2 > *radian || *radian > (PI / 2) * 3)
 	{
 		rc->x_len = 1 - rc->x_len;
 		rc->x_dir = 1;
 	}
-	if (radian < PI)
+	if (*radian < PI)
 	{
 		rc->y_len = 1 - rc->y_len;
 		rc->y_dir = 1;
 	}
-	rc->x_asix_near_rdist = 1 / sin(radian) * rc->y_len * rc->y_dir;
-	rc->y_asix_near_rdist = 1 / cos(radian) * rc->x_len * rc->x_dir;
-	rc->x_one_move_rdist = 1 / cos(radian) * 1 * rc->x_dir;
-	rc->y_one_move_rdist = 1 / sin(radian) * 1 * rc->y_dir;
+	rc->x_asix_near_rdist = 1 / sin(*radian) * rc->y_len * rc->y_dir;
+	rc->y_asix_near_rdist = 1 / cos(*radian) * rc->x_len * rc->x_dir;
+	rc->x_one_move_rdist = 1 / cos(*radian) * 1 * rc->x_dir;
+	rc->y_one_move_rdist = 1 / sin(*radian) * 1 * rc->y_dir;
 }
 
 static void	set_rpos(t_player_data *player, t_raycast *rc, double radian, double *rpos)
@@ -63,7 +67,7 @@ double	ray_cast_distance(t_cub3d *p_data, double radian, double *rpos)
 
 	player = &p_data->player;
 	content = p_data->content_data.content_ptr;
-	init_rc(&rc, player, radian);
+	init_rc(&rc, player, &radian);
 	while (content[(int)(player->pos.y + (rc.y_step) * rc.y_dir)] \
 				[(int)(player->pos.x + (rc.x_step) * rc.x_dir)] != 1)
 	{
